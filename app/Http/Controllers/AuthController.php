113379bc24794bbe6b4c49cf2506a9f40a35aba2
Husen Efendi <husenef@gmail.com>
2019-03-29 20:26:31 +0700

    add create table user && auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function register(Request $request)
+    { }
+    //login
+    public function login(Request $request)
+    { }
+}
