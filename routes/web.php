657cd0693a8a529fb80ef7684d74aecd1d547b5c
Husen Efendi <husenef@gmail.com>
2019-04-03 06:02:20 +0700

    prepare done

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+$router->get("/", function () use ($router) {
+    return view('main');
+});
+
+$router->get('/version', function () use ($router) {
     return $router->app->version();
 });
+
 $router->get("/key", function () {
     return str_random(32);
 });
