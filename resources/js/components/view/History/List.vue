67dc14a845b00f7e17a4f9fb71e3ad1307246c2c
Husen Efendi <husenef@gmail.com>
2019-04-11 22:37:50 +0700

    tinggal looping history

--- resources/js/components/view/History/List.vue
+++ resources/js/components/view/History/List.vue
@@ -1,5 +1,24 @@
 <template>
-  <h2>history list</h2>
+  <div class="card">
+    <div class="card-header">
+      <h2>List</h2>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Data Name</th>
+              <th>Status</th>
+              <th>Raw</th>
+              <th>#</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,6 +36,10 @@ export default {
     getList() {
       axios.get("/api/v1/history/list").then(res => {
         console.log("ress", res.data);
+        if (res.data.success) {
+          const histId = Object.keys(res.data.data).filter(e => e != "meta");
+          this.list = histId.map(i => res.data.data[i]);
+        }
       });
     }
   }
