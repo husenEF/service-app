1209d29e8ee02a10c2ef6b63dd59ba31b96e2155
Husen Efendi <husenef@gmail.com>
2019-04-18 05:55:58 +0700

    update edit user

--- resources/js/components/view/User/Edit.vue
+++ resources/js/components/view/User/Edit.vue
@@ -8,18 +8,38 @@
         </p>
       </div>
       <form @submit.prevent="submitData">
-        <div class="form-group">
-          <label>Name</label>
-          <input type="text" class="form-control" v-model="user.name">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Name</label>
+              <input type="text" class="form-control" v-model="user.name">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Email</label>
+              <input type="email" class="form-control" v-model="user.email">
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-          <label>Email</label>
-          <input type="email" class="form-control" v-model="user.email">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control" v-model="user.password">
+            </div>
+          </div>
+          <div class="col-md-6" v-if="user.roles=='admin'">
+            <div class="form-group">
+              <label>Jabatan</label>
+              <select v-model="user.roles" class="form-control">
+                <option value="mekanik">Mekanik</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-          <label>Password</label>
-          <input type="password" class="form-control" v-model="user.password">
-        </div>
+
         <div class="form-group">
           <div class="form-check">
             <input type="checkbox" id="checkbox" v-model="user.active" class="form-check-input">
