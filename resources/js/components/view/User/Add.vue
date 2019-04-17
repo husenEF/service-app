a4d3e87e2b0f1d4a40fbc396ab72eb4a3cb1fbb7
Husen Efendi <husenef@gmail.com>
2019-04-18 06:06:11 +0700

    add form user

--- resources/js/components/view/User/Add.vue
+++ resources/js/components/view/User/Add.vue
@@ -1,7 +1,49 @@
 <template>
   <div class="card">
-    <div class="card-header"></div>
-    <div class="card-body"></div>
+    <div class="card-header">Add New User</div>
+    <div class="card-body">
+      <form @submit.prevent="submitData">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Name</label>
+              <input type="text" class="form-control" v-model="user.name">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Email</label>
+              <input type="email" class="form-control" v-model="user.email">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control" v-model="user.password">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Jabatan</label>
+              <select v-model="user.roles" class="form-control">
+                <option value="mekanik">Mekanik</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="form-check">
+            <input type="checkbox" id="checkbox" v-model="user.active" class="form-check-input">
+            <label class="form-check-label" for="checkbox">Active User</label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -11,8 +53,27 @@ export default {
   created() {
     this.$emit("back", "/user");
   },
-  methods:{
-
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        roles: "mekanik",
+        active: false
+      }
+    };
+  },
+  methods: {
+    submitData() {
+      axios
+        .post("/api/v1/user/create", this.user)
+        .then(res => {
+          console.log("res", res);
+        })
+        .catch(err => {
+          console.log("err", err);
+        });
+    }
   }
 };
 </script>
