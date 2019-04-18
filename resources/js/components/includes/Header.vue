802da051d0e7ff0d124860cb2bdaef5df93c261b
husen <husen@qwords.co.id>
2019-04-18 09:00:27 +0700

    add logout

--- resources/js/components/includes/Header.vue
+++ resources/js/components/includes/Header.vue
@@ -3,15 +3,23 @@
     <h1 class="text-center w-100">
       <router-link :to="{path:back}" class="float-left" v-if="isBack">Back</router-link>
       {{title}}
+      <button class="btn btn-sm btn-danger float-right" v-on:click="logout()">
+        <LogOutIcon/>
+      </button>
     </h1>
   </nav>
 </template>
 
 <script>
+import { LogOutIcon } from "vue-feather-icons";
+
 export default {
   name: "header",
   props: ["title", "back"],
   computed: {},
+  components: {
+    LogOutIcon
+  },
   created() {
     this.isBack = this.back !== "" ? true : false;
   },
@@ -22,6 +30,19 @@ export default {
     return {
       isBack: true
     };
+  },
+  computed: {
+    user() {
+      return this.$store.getters.currentUser;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit("logout");
+      this.$swal("Anda sudah Logout").then(val => {
+        this.$router.push("/login");
+      });
+    }
   }
 };
 </script>
