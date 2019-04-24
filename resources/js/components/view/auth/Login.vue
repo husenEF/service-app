286b6cc8304868d5e508c58bd024eb4c06b1db55
Husen Efendi <husenef@gmail.com>
2019-04-24 18:57:11 +0700

    updare to window location

--- resources/js/components/view/auth/Login.vue
+++ resources/js/components/view/auth/Login.vue
@@ -9,11 +9,23 @@
             <form @submit.prevent="authenticate">
               <div class="form-group">
                 <label for="username">Username</label>
-                <input type="text" name="username" class="form-control" id="username" v-model="form.username">
+                <input
+                  type="text"
+                  name="username"
+                  class="form-control"
+                  id="username"
+                  v-model="form.username"
+                >
               </div>
               <div class="form-group">
                 <label for>Password</label>
-                <input type="password" name="password" class="form-control" id="password" v-model="form.password">
+                <input
+                  type="password"
+                  name="password"
+                  class="form-control"
+                  id="password"
+                  v-model="form.password"
+                >
               </div>
               <div class="form-group">
                 <button type="submit" class="btn btn-primary">Login</button>
@@ -56,7 +68,8 @@ export default {
         // console.log("res", res);
         if (res.success) {
           this.$store.commit("loginSuccess", res.data);
-          this.$router.push({ path: "/" });
+          window.location = "/";
+          // this.$router.push({ path: "/" });
         } else {
           this.$store.commit("loginFailed", { error: res.message });
         }
