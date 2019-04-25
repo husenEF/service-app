911ecd67c6aac5b6f96ab0db57bafad37d8f81f3
husen <husen@qwords.co.id>
2019-04-25 08:34:59 +0700

    update table user

--- resources/js/components/view/Tire/List.vue
+++ resources/js/components/view/Tire/List.vue
@@ -187,4 +187,67 @@ export default {
     }
   }
 }
+
+@media only screen and (max-width: 760px),
+  (min-device-width: 768px) and (max-device-width: 1024px) {
+  /* Force table to not be like tables anymore */
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+
+  thead {
+    tr {
+      position: absolute;
+      top: -9999px;
+      left: -9999px;
+    }
+  }
+  tbody {
+    tr {
+      border-left: 1px #eee solid;
+      border-right: 1px #eee solid;
+      &:nth-child(odd) {
+        background-color: #dedede;
+      }
+    }
+    td {
+      /* Behave  like a "row" */
+      border: none;
+      border-bottom: 1px solid #eee;
+      position: relative;
+      padding-left: 50%;
+      &::before {
+        /* Now like a table header */
+        position: absolute;
+        /* Top/left values mimic padding */
+        top: 6px;
+        left: 6px;
+        width: 45%;
+        padding-right: 10px;
+        white-space: nowrap;
+      }
+    }
+  }
+
+  td:nth-of-type(1):before {
+    content: "No";
+  }
+  td:nth-of-type(2):before {
+    content: "Merek";
+  }
+  td:nth-of-type(3):before {
+    content: "Tanggal Beli";
+  }
+  td:nth-of-type(4):before {
+    content: "Kendaraan";
+  }
+  td:nth-of-type(5):before {
+    content: "Riwayat Penggunaan";
+  }
+}
 </style>
