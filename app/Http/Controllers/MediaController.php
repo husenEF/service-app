2a626084a81faf5bbdc464bb87d3cb9a0a05ee9e
Husen Efendi <husenef@gmail.com>
2019-05-04 19:28:49 +0700

    update media controller

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Response;
 
 class MediaController extends Controller
 {
     function get($type, $filename)
     {
-        // dd([$type, $filename]);
         $path = storage_path('app/public/' . $type . '/' . $filename);
         if (!File::exists($path)) {
-            // return view()
             return response("404 not found", 404);
         }
+        $ext = File::extension($path);
+        $get = File::get($path);
         
+        return response($get)
+            ->header('Content-Type', "image/$ext")
+            ->header('Pragma', 'public')
+            ->header('Content-Disposition', 'inline; filename="' . $filename . '"')
+            ->header('Cache-Control', 'max-age=60, must-revalidate');
     }
 }
