06bf07a007bb1b65acac948b6296aade228da955
Husen Efendi <husenef@gmail.com>
2019-05-12 11:17:22 +0700

    update vehicle export with heder + mapping data

--- app/Exports/VehicleExport.php
+++ app/Exports/VehicleExport.php
@@ -2,16 +2,40 @@
 
 namespace App\Exports;
 
+use Carbon\Traits\Date;
 use App\Vehicle;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class VehicleExport implements FromCollection
+class VehicleExport implements FromCollection, WithHeadings, WithMapping
 {
+    public function headings(): array
+    {
+        return [
+            'id',
+            'Nama Creator',
+            'Merek',
+            "size",
+            "platnumber",
+        ];
+    }
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Vehicle::all();
+    }
+    public function map($vehicle): array
+    {
+        return [
+            $vehicle->id,
+            $vehicle->user->name,
+            $vehicle->merek,
+            $vehicle->size,
+            $vehicle->platnumber,
+            // Date::dateTimeToExcel($vehicle->created_at),
+        ];
     }
 }
