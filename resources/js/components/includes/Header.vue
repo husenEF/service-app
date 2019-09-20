ded98f463822c7e03928ede199b2ebd7c0827a9e
Husen Efendi <husenef@gmail.com>
2019-09-20 09:47:46 +0700

    refactor script

--- resources/js/components/includes/Header.vue
+++ resources/js/components/includes/Header.vue
@@ -1,56 +1,62 @@
 <template>
-    <nav class="navbar fixed-sticky mb-5 w-100 main-nav">
-        <h1 class="text-center w-100 h4 mb-0">
-            <router-link :to="{path:back}" class="float-left btn btn-sm btn-default" v-if="isBack"><ArrowLeftIcon/></router-link>
-            {{title}}
-            <button class="btn btn-sm btn-default float-right" v-on:click="logout()" title="Logout">
-                <LogOutIcon/>
-            </button>
-        </h1>
-    </nav>
+  <nav class="navbar fixed-sticky mb-5 w-100 main-nav">
+    <h1 class="text-center w-100 h4 mb-0">
+      <router-link :to="{path:back}" class="float-left btn btn-sm btn-default" v-if="isBack">
+        <ArrowLeftIcon />
+      </router-link>
+      {{title}}
+      <button
+        class="btn btn-sm btn-default float-right"
+        v-on:click="logout()"
+        title="Logout"
+      >
+        <LogOutIcon />
+      </button>
+    </h1>
+  </nav>
 </template>
 
 <script>
-    import { LogOutIcon, ArrowLeftIcon } from "vue-feather-icons";
+import { LogOutIcon, ArrowLeftIcon } from "vue-feather-icons";
 
-    export default {
-        name: "header",
-        props: ["title", "back"],
-        computed: {},
-        components: {
-            LogOutIcon,
-            ArrowLeftIcon
-        },
-        created() {
-            this.isBack = this.back !== "" ? true : false;
-        },
-        mounted() {
-            // console.log("this", this.isBack);
-        },
-        data() {
-            return {
-                isBack: true
-            };
-        },
-        computed: {
-            user() {
-                return this.$store.getters.currentUser;
-            }
-        },
-        methods: {
-            logout() {
-                this.$store.commit("logout");
-                this.$swal("Anda sudah keluar").then(val => {
-                    this.$router.push("/login");
-                });
-            }
-        }
+export default {
+  name: "header",
+  props: ["title", "back"],
+  computed: {},
+  components: {
+    LogOutIcon,
+    ArrowLeftIcon
+  },
+  created() {
+    this.isBack = this.back !== "" ? true : false;
+  },
+  mounted() {
+    // console.log("this", this.isBack);
+  },
+  data() {
+    return {
+      isBack: true
     };
+  },
+  computed: {
+    user() {
+      return this.$store.getters.currentUser;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit("logout");
+      this.$swal("Anda sudah keluar").then(val => {
+        this.$router.push("/login");
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .main-nav {
-        background-color: #dddddd;
-    }
+.main-nav {
+  background-color: #dddddd;
+}
 </style>
 
